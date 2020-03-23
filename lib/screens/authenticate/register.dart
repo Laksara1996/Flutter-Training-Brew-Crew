26f72f90dc9a61b141d9947bbe1ae207f6a3ba24
Laksara <40088875+Laksara1996@users.noreply.github.com>
2020-03-23 18:49:15 +0530

    Register Forum

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,22 +1,20 @@
-import 'package:brewcrew/screens/authenticate/register.dart';
-import 'package:brewcrew/services/auth.dart';
 import 'package:flutter/material.dart';
+import '../../services/auth.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
 
   final Function toggleView;
 
-  SignIn ({this.toggleView});
+  Register ({this.toggleView});
 
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
 
-  //Text Field State
   String email = '';
   String password = '';
 
@@ -27,14 +25,14 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
+        title: Text('Sign up to Brew Crew'),
         actions: <Widget>[
           FlatButton.icon(
               onPressed: () {
                 widget.toggleView();
               },
               icon: Icon(Icons.person),
-              label: Text('Register')
+              label: Text('Sign in')
           )
         ],
       ),
@@ -64,9 +62,9 @@ class _SignInState extends State<SignIn> {
               RaisedButton(
                 color: Colors.pink[400],
                 child: Text(
-                    'Sign In',
+                  'Sign up',
                   style: TextStyle(
-                    color: Colors.white
+                      color: Colors.white
                   ),
                 ),
                 onPressed: () async {
