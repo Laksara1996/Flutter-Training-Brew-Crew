cd09f31f100b71d511d4acf011bd2c811b39a876
Laksara <40088875+Laksara1996@users.noreply.github.com>
2020-03-25 18:49:31 +0530

    Listing Data

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,9 @@
+import 'package:brewcrew/models/brew.dart';
 import 'package:brewcrew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../services/database.dart';
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
 
@@ -7,21 +11,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Brew Crew'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('logout')
-          )
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('logout')
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,6 @@
+import 'package:brewcrew/main.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/brew.dart';
 
 class DatabaseService {
 
@@ -15,6 +17,23 @@ class DatabaseService {
       'name': name,
       'strength': strength,
     });
+  }
+
+  //brew list from snapshot
+  List<Brew> _brewListFromSnapSHot (QuerySnapshot snapshot){
+    return snapshot.documents.map((doc) {
+      return Brew(
+      name: doc.data['name'] ?? '',
+      sugars: doc.data['sugars'] ?? '0',
+      strength: doc.data['strength'] ?? 0
+      );
+    }).toList();
+  }
+
+  //get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots()
+    .map(_brewListFromSnapSHot);
   }
 
 }
